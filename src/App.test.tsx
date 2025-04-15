f3099b60e3cdb946048542f5ee5b59c84679deff
Roman Gravit <romang@siber.com>
2025-04-15 17:52:15 +0100

    - add get/find/query tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,33 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('renders the App component', () => {
+  it('get by Text', () => {
     render(<App />)
 		const msg = screen.getByText(/hello/i);
 		expect(msg).toBeInTheDocument();
   })
+
+	it('find async', async () => {
+		render(<App />);
+		const div = await screen.findByText("async");
+		expect(div).toBeInTheDocument();
+	})
+
+	it('get role', () => {
+		render(<App />);
+		const heading = screen.getByRole("heading");
+		expect(heading).toBeInTheDocument();
+	})
+
+	it('get Alt', () => {
+		render(<App />);
+		const img = screen.getByAltText(/alt text/i);
+		expect(img).toBeInTheDocument();
+	})
+
+	it('get placeholder', () => {
+		render(<App />);
+		const place = screen.getByPlaceholderText(/user/i);
+		expect(place).toBeInTheDocument();
+	})
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData("async");
+    }, 500);
+  }, [])
 
   return (
     <>
       <h1>Hello world</h1>
+      <img src='null' alt="alt text" />
+      {data  &&  <div style={{color: 'red'}} className="div-data">{data}</div>}
+      <button>Click me</button>
+      <input type="text" placeholder="user" />
     </>
   )
 }
